9005061cdce5ce0676b379ed9333df7827bc2219
Yashaswini-Lankalapalli <158067195+Yashaswini-Lankalapalli@users.noreply.github.com>
2025-02-23 15:00:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ export default function Footer(){
     return (
         <div className="footer container">
             <div className="footer-section">
-                <p className="title">FoodiesHub.com</p>
-                <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
+                <p className="title">CookLook.com</p>
+                <p>CookLook is your go-to destination for mouthwatering recipes from all cuisines, bringing joy to your taste buds and soul. And the best part? It’s completely free! </p>
                 <p>&copy; 2021 | All Rights Reserved</p>
             </div>
             <div className="footer-section">
                 <p className="title">Contact Us</p>
-                <p>foodieshub@gmail.com</p>
+                <p>cooklook@gmail.com</p>
                 <p>+342-5324-9454</p>
                 <p>2393 Street NYC</p>
             </div>
